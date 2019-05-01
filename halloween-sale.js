ad3841b85b672dd7ca07548bc4c1a2b09ac2e706
Edward Iga Kigongo <igakigongo@gmail.com>
2019-05-01 23:15:23 +0200

    Halloween Sale

---
+++ halloween-sale.js
@@ -0,0 +1,16 @@
+const howManyGames = (p, d, m, s) => {
+	let count = 0,
+		price = p,
+		sum = 0;
+
+	while (sum <= s) {
+		sum += price;
+		count += sum <= s ? 1 : 0;
+		price = price - d > m ? price - d : m;
+	}
+
+	return count;
+}
+
+console.log(howManyGames(20, 3, 6, 80));
+console.log(howManyGames(20, 3, 6, 85));
