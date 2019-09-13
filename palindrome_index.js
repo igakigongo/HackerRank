28a42f591355c768e13bcec77d767c3098abd4ad
Edward Iga Kigongo <igakigongo@gmail.com>
2019-09-13 03:59:07 +0200

    Palindrome Index - Remove a character to make a string palindrome

---
+++ palindrome_index.js
@@ -0,0 +1,32 @@
+const subStrIsPalindrome = (str, left, right) => {
+  while (left <= right) {
+    if (str[left] !== str[right]) return false;
+    left++;
+    right--;
+  }
+  return true;
+};
+
+const palindromeIndex = (str) => {
+  let left = 0, right = str.length - 1;
+  while (left <= right) {
+    if (str[left] !== str[right]) {
+      if (subStrIsPalindrome(str, left + 1, right)) return left;
+      if (subStrIsPalindrome(str, left, right - 1)) return right;
+    }
+    left++;
+    right--;
+  }
+  return -1;
+}
+
+// Test cases
+console.log(`"a" is palindrome? ${palindromeIndex("a")}`);
+console.log(`"abab" is palindrome? ${palindromeIndex("abab")}`);
+console.log(`"abab" is palindrome? ${palindromeIndex("abab")}`);
+console.log(`"aaab" is palindrome? ${palindromeIndex("aaab")}`);
+console.log(`"pap" is palindrome? ${palindromeIndex("pap")}`);
+
+
+// "a, b, b, a, e, c, a, b, b, a"
+//  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
