5bb43952d88bd8bdc0a562c05338869766069594
Iga Kigongo Edward <igakigongo@gmail.com>
2022-04-01 23:09:12 +0200

    update count-apples-and-oranges, replace ternary operator and remove intermediate variables

--- src/count-apples-and-oranges.js
+++ src/count-apples-and-oranges.js
@@ -1,10 +1,8 @@
-export const totalInRange = (s, t, treeLocation, array) => {
-  const result = array.reduce((a, c) => {
+export const totalInRange = (s, t, treeLocation, array) => array
+  .reduce((a, c) => {
     const fruitPosition = c + treeLocation;
-    return a + ((fruitPosition >= s && fruitPosition <= t) ? 1 : 0);
+    return a + (fruitPosition >= s && fruitPosition <= t) * 1;
   }, 0);
-  return result;
-};
 
 const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
   const totalApplesInRange = totalInRange(s, t, a, apples);
