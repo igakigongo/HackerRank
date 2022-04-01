821671a944dee091fb6cebe90e7e8f0b4a9509a8
Iga Kigongo Edward <igakigongo@gmail.com>
2022-04-01 22:40:51 +0200

    update angry professor

--- src/angry-professor.js
+++ src/angry-professor.js
@@ -2,12 +2,9 @@ export const YES = 'YES';
 export const NO = 'NO';
 
 const cancelled = (threshold, arrivalTimes) => {
-  let early = 0;
-  for (let i = 0; i < arrivalTimes.length; i += 1) {
-    early += arrivalTimes[i] <= 0 ? 1 : 0;
-    if (early >= threshold) {
-      return NO;
-    }
+  for (let i = 0, early = 0; i < arrivalTimes.length; i += 1) {
+    early += (arrivalTimes[i] <= 0) * 1;
+    if (early >= threshold) return NO;
   }
   return YES;
 };
